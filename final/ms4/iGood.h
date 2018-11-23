47f9b8c24d6445fd0e0dbbb697bfa5225beee72b
shnam16 <shnam16@gmail.com>
2018-11-22 20:40:43 -0500

    ms4

---
+++ final/ms4/iGood.h
@@ -0,0 +1,42 @@
+//Student name: Seung Ho Nam
+//Student number: 148151178
+//Date: 2018/11/25
+
+#ifndef AID_IGOOD_H
+#define AID_IGOOD_H
+
+#include <iostream>
+#include <fstream>
+
+namespace aid {
+
+  class iGood {
+
+  public:
+    
+    virtual std::fstream& store(std::fstream& file, bool newline = true) const = 0;
+    virtual std::fstream& load(std::fstream& file) = 0;
+    virtual std::ostream& write(std::ostream& os, bool linear) const = 0;
+    virtual std::istream& read(std::istream& is) = 0;
+    virtual bool operator==(const char*) const = 0;
+    virtual double total_cost() const = 0;
+    virtual const char* name() const = 0;
+    virtual void quantity(int) = 0;
+    virtual int qtyNeeded() const = 0;
+    virtual int quantity() const = 0;
+    virtual int operator+=(int) = 0;
+    virtual bool operator>(const iGood&) const = 0;
+    virtual ~iGood() {};
+
+  };
+
+  std::ostream& operator<<(std::ostream&, const iGood&);
+  std::istream& operator>>(std::istream&, iGood&);
+  double operator+=(double&, const iGood&);
+  iGood* CreateGood();
+  iGood* CreatePerishable();
+
+}
+
+#endif // !AID_IGOOD_H
+
